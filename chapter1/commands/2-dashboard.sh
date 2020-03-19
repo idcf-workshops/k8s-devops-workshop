46b267f7520a48455dd85f96786e7b55a5c6711b
Jijie Chen <jijie.chen@someplace.com>
2020-03-19 18:29:51 +0800

    修复了安装 dashboard 用的文件位置

--- chapter1/commands/2-dashboard.sh
+++ chapter1/commands/2-dashboard.sh
@@ -3,7 +3,7 @@ kubectl config set-context $(kubectl config current-context) --namespace
 
 安装 dashboard（40s）
     kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta8/aio/deploy/recommended.yaml
-    # kubectl apply -f https://gitee.com/idcf-devops-on-kubernetes/chapter1/raw/master/assets/k8s-dashboard.yaml
+    # kubectl apply -f https://gitee.com/idcf-devops-on-kubernetes/workshop-assets/raw/master/chapter1/assets/k8s-dashboard.yaml
 
 启用代理：
     kubectl proxy
