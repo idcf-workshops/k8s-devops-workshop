7b1af34ca34afe1e1b27e7aa89ce5c186d88cfa0
Jijie Chen <jijie.chen@someplace.com>
2020-03-30 10:53:57 +0800

    增加了给 boat-house 部署用的 helm chart 源码

--- chapter4/deploy-infra.sh
+++ chapter4/deploy-infra.sh
@@ -4,21 +4,21 @@ set -e
 
 ARG_1=$1
 
-ITEMS='product-service-db statistics-service-db statistics-service-redis'
+ITEMS='product-service-db,statistics-service-db,statistics-service-redis'
 ENV=test
 if [ ! -z $ARG_1 ] ; then
     ENV=$ARG_1
 fi
 
-DB_INIT_SCRIPT='./product-service/api/scripts/init.sql'
-SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+DB_INIT_SCRIPT='./assets/init-product-service-db.sql'
+
 
 kubectl create namespace boathouse-$ENV 2>/dev/null || true
-for ITEM in $ITEMS ; do
-    $SCRIPTPATH/4-deploy-service.sh $ITEM whatever $ENV
-done
+helm install infra ./boat-house-chart --namespace boathouse-$ENV \
+        --set "services={$ITEMS},imageNamePrefix=boat-house-,imageTag=workshop,imageRepository=ccr.ccs.tencentyun.com/idcf-k8s-devops"
 
-kubectl rollout status deploy/product-service-db-deployment -n boathouse-$ENV
+
+kubectl rollout status deploy/product-service-db -n boathouse-$ENV
 
 PRODUCT_DB_POD=$(kubectl get pods -o=jsonpath='{.items[0].metadata.name}' -l app=product-service-db)
 sleep 10
