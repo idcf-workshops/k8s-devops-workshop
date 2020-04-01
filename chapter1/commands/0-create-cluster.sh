1734379161e7733b59ec3ea23d5a6899de912409
Jijie Chen <jijie.chen@someplace.com>
2020-04-01 09:01:50 +0800

    添加了用于安装 efk 和 prometheus 及 grafana 的 helm 配置

--- chapter1/commands/0-create-cluster.sh
+++ chapter1/commands/0-create-cluster.sh
@@ -1,6 +1,6 @@
 
-Minikube 命令
-    
+# Minikube 命令
+minikube start --cpus 4 --memory 8192   
 
-Azure Cloud 创建命令
-    
+# Azure Cloud 创建命令
+az aks create
---
+++ chapter5/commands.sh
@@ -0,0 +1,23 @@
+
+
+# 绑定 Helm 依赖
+# helm repo add stable https://mirror.azure.cn/kubernetes/charts/
+# helm repo add kiwigrid https://kiwigrid.github.io/
+# helm dependency build ./logging
+# helm dependency build ./monitoring
+
+kubectl create namespace logging
+helm install logging -n logging ./logging
+
+
+kubectl create namespace monitoring
+helm install monitoring -n monitoring ./monitoring
+
+
+# curl -O -L https://mirror.azure.cn/kubernetes/charts-incubator/metrics-server-2.0.4.tgz && tar -zxf metrics-server-2.0.4.tgz && rm metrics-server-2.0.4.tgz
+# helm template metrics-server ./metrics-server --set 'image.repository=gcr.azk8s.cn/google_containers/metrics-server-amd64,args={--kubelet-insecure-tls}' -n kube-system | kubectl apply -f -
+
+
+# Dashboard: Cluster Monitoring for Kubernetes
+# k8s-app-metrics.json
+# node -p 'while(true){}'
