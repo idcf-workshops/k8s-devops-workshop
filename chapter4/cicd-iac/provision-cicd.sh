1e02848930ea621cdfc31a6d624e1aab42475812
Jijie Chen <jijie.chen@someplace.com>
2020-04-03 21:36:09 +0800

    在 provision-cicd.sh 中使用 helm 安装

--- chapter4/cicd-iac/provision-cicd.sh
+++ chapter4/cicd-iac/provision-cicd.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "##################################"
-echo "#  MAKE SURE YOU ARE LOGGED IN:  #"
-echo "#  kubectl cluster-info   #"
-echo "##################################"
-
 function usage() {
     echo
     echo "Usage:"
@@ -15,8 +10,8 @@ function usage() {
     echo " $0 deploy"
     echo
     echo "COMMANDS:"
-    echo "   deploy         Set up the demo projects and deploy demo apps"
-    echo "   delete         Clean up and remove demo projects and objects"
+    echo "   deploy       部署 CI CD 环境"
+    echo "   delete       删除 CI CD 环境"
     echo
 }
 
@@ -49,81 +44,28 @@ while :; do
     shift
 done
 
-
-################################################################################
-# CONFIGURATION                                                                #
-################################################################################
-
-
-
-NAMESPACES=$(kubectl get namespaces)
-
-create_ns(){
-    local NAME
-    NAME=$1
-    EXISTS=$(echo $NAMESPACES | grep $NAME)
-    if [ -z "$EXISTS" ]; then
-        kubectl create namespace $NAME || true
-    fi
-}
-
-#   jenkins: 0.5G 2G
-#   gogs: 0.5G 1G
-#   sonarqube: 1.25G 2.5G
-#   nexus: 0.5G 2Gi
-function deploy() {
-    create_ns cicd-iac
-
-  sleep 2
-
-  echo 'Provisioning applications...'
-  kcd cicd-iac
-  
-  ./tmpl.sh ./manifests/jenkins.yaml ./manifests/vars | kubectl apply -f -
-  sleep 3
-
-  ./tmpl.sh ./manifests/sonarqube.yaml ./manifests/vars | kubectl apply -f -
-  sleep 3
-
-  ./tmpl.sh ./manifests/nexus.yaml ./manifests/vars | kubectl apply -f -
-  sleep 3
-
-  ./tmpl.sh ./manifests/gogs.yaml ./manifests/vars | kubectl apply -f -
-  sleep 3
-
-  echo "Provisioning installer"
-  ./tmpl.sh ./manifests/cicd-installer.yaml ./manifests/vars | kubectl apply -f -
-
-  echo "Wait for installing..."
-  sleep 3
-  kubectl wait --for=condition=complete --timeout=900s job/cicd-installer
-
-  kubectl logs pods/$(kubectl get pods -o=jsonpath='{.items[0].metadata.name}' -l job-name=cicd-installer)
-}
-
 function kcd() {
   kubectl config set-context $(kubectl config current-context) --namespace $1
 }
 
-function echo_header() {
-  echo
-  echo "##############################"
-  echo $1
-  echo "##############################"
+function deploy() {
+  EXISTS=$(echo $NAMESPACES | grep cicd-iac)
+    if [ -z "$EXISTS" ]; then
+        kubectl create namespace cicd-iac || true
+        sleep 2
+    fi  
+  
+  kcd cicd-iac
+  echo "正在等待部署完成，请稍候..."
+  helm install lab ./ -n cicd-iac --set 'imagePushRegistry.location=ccr.ccs.tencentyun.com/idcf-k8s-devops,imagePushRegistry.username=100000627191,imagePushRegistry.password=odGJpN4onmC'
 }
-
-################################################################################
-# MAIN: DEPLOY Workshop                                                   #
-################################################################################
-
 
 START=`date +%s`
 
-echo_header "DevOps on Kubernetes ($(date))"
-
 case "$ARG_COMMAND" in
     delete)
-        echo "Delete demo..."
+        echo "Deleting..."
+        helm uninstall lab
         kubectl delete namespace/cicd-iac
         echo
         echo "Delete completed successfully!"
@@ -135,7 +77,6 @@ case "$ARG_COMMAND" in
         deploy
         echo
         echo "Provisioning completed successfully!"
-        kcd cicd-iac
         ;;
         
     *)
